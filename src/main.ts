150a7b4a6ef27b60d2e5a2c67fec7560dd319850
Pratik <rajpratik001@gmail.com>
2024-02-25 14:26:07 +0530

    main modified

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { hostname } from 'os';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: true,
     credentials: true,
   });
 
